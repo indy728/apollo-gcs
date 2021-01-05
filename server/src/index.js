1b2e8953a5469d026d97333980d47db5a91dc157
indy728 <kdevlinmurray@gmail.com>
2021-01-05 15:52:12 -0800

    server up and running

---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+const {ApolloServer, gql} = require('apollo-server-express');
+const {existsSync, mkdirSync} = require('fs');
+const path = require('path');
+const express = require('express');
+const {typeDefs, resolvers} = require('./schema');
+
+const files = [];
+
+existsSync(path.join(__dirname, "img")) || mkdirSync(path.join(__dirname, "images"));
+
+const server = new ApolloServer({typeDefs, resolvers});
+const app = express();
+
+app.use('./img', express.static(path.join(__dirname, 'img')));
+server.applyMiddleware({app});
+
+app.listen(4000, () => {
+  console.log(`🚀  Server ready at http://localhost:4000/`);
+});
---
+++ server/src/schema.js
@@ -0,0 +1,38 @@
+const {gql} = require('apollo-server-express');
+const {createWriteStream, existsSync, mkdirSync} = require('fs');
+const path = require('path');
+
+const typeDefs = gql`
+ type Query {
+   files: [String]
+ }
+
+ type Mutation {
+   uploadFile(file: Upload!): Boolean
+ }
+`
+
+const resolvers = {
+  Query: {
+    files: () => files
+  },
+  Mutation: {
+    uploadFile: async(_, { file }) => {
+      const { createReadStream, filename } = await file;
+
+      await new Promise(resolve => 
+        createReadStream()
+          .pipe(createWriteStream(path.join(__dirname, 'img', filename)))
+          .on('close', resolve)  
+      )
+
+      files.push(filename);
+
+      return true;
+    }
+  }
+
+}
+
+exports.typeDefs = typeDefs;
+exports.resolvers = resolvers;
