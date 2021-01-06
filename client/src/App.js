9e10f8963fa8f0ad95fe63dfea1b06e2f9296e86
indy728 <kdevlinmurray@gmail.com>
2021-01-05 18:08:33 -0800

    apollo app frontend first page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from "react";
+// import { Upload } from "./Upload";
+// import { Files } from "./Files";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
+      {/* <Upload />
+      <Files /> */}
     </div>
   );
 }
